ca4e9f40a6e537178081e174b2ea0e716ccc19d6
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2021-02-10 00:07:26 +0530

    :tada: completed learning graphQL subscriptions with apollo-link-ws

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -1,34 +1,41 @@
-import React, { Component } from 'react';
-import { addMessage, getMessages } from './graphql/queries';
-import MessageInput from './MessageInput';
-import MessageList from './MessageList';
+import * as React from "react";
+import { addMessage, getMessages, onMessageAdded } from "./graphql/queries";
+import MessageInput from "./MessageInput";
+import MessageList from "./MessageList";
 
-class Chat extends Component {
-  state = {messages: []};
+export default function Chat({ user }) {
+  const [messages, setMessages] = React.useState([]);
+  const subscriptionRef = React.useRef(null);
 
-  async componentDidMount() {
-    const messages = await getMessages();
-    this.setState({messages});
-  }
+  React.useEffect(() => {
+    async function fetchMessages() {
+      const messages = await getMessages();
+      setMessages(messages);
+    }
+    fetchMessages();
+    return () => {
+      console.log("Unsubscribing chat events...");
+      subscriptionRef.current.unsubscribe();
+    };
+  }, []);
 
-  async handleSend(text) {
-    const message = await addMessage(text);
-    this.setState({messages: this.state.messages.concat(message)});
-  }
-
-  render() {
-    const {user} = this.props;
-    const {messages} = this.state;
-    return (
-      <section className="section">
-        <div className="container">
-          <h1 className="title">Chatting as {user}</h1>
-          <MessageList user={user} messages={messages} />
-          <MessageInput onSend={this.handleSend.bind(this)} />
-        </div>
-      </section>
+  React.useEffect(() => {
+    subscriptionRef.current = onMessageAdded((message) =>
+      setMessages([...messages, message])
     );
-  }  
-}
+  }, [messages]);
 
-export default Chat;
+  const handleSend = async (text) => {
+    await addMessage(text);
+  };
+
+  return (
+    <section className="section">
+      <div className="container">
+        <h1 className="title">Chatting as {user}</h1>
+        <MessageList user={user} messages={messages} />
+        <MessageInput onSend={handleSend} />
+      </div>
+    </section>
+  );
+}
--- client/src/graphql/queries.js
+++ client/src/graphql/queries.js
@@ -1,5 +1,5 @@
-import gql from 'graphql-tag';
-import client from './client';
+import gql from "graphql-tag";
+import client from "./client";
 
 const messagesQuery = gql`
   query MessagesQuery {
@@ -21,15 +21,32 @@ const addMessageMutation = gql`
   }
 `;
 
+const messageAddedSubscription = gql`
+  subscription MessageAdded {
+    messageAdded {
+      id
+      from
+      text
+    }
+  }
+`;
+
 export async function addMessage(text) {
-  const {data} = await client.mutate({
+  const { data } = await client.mutate({
     mutation: addMessageMutation,
-    variables: {input: {text}}
+    variables: { input: { text } },
   });
   return data.message;
 }
 
 export async function getMessages() {
-  const {data} = await client.query({query: messagesQuery});
+  const { data } = await client.query({ query: messagesQuery });
   return data.messages;
+}
+
+export function onMessageAdded(handleChange) {
+  // subscribe to messages from server
+  const observable = client.subscribe({ query: messageAddedSubscription });
+  // subscribe the UI to state changes.
+  return observable.subscribe(({ data }) => handleChange(data.messageAdded));
 }
