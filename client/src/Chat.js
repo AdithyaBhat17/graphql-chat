b1d5c99bbc1a84824a70e26dbe0753858c78a5d1
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2021-02-10 21:52:55 +0530

    🎉 Completed migrating to Apollo react hooks!

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -1,33 +1,45 @@
+import { useMutation, useQuery, useSubscription } from "@apollo/react-hooks";
 import * as React from "react";
-import { addMessage, getMessages, onMessageAdded } from "./graphql/queries";
+import {
+  addMessageMutation,
+  messageAddedSubscription,
+  messagesQuery,
+} from "./graphql/queries";
 import MessageInput from "./MessageInput";
 import MessageList from "./MessageList";
 
 export default function Chat({ user }) {
-  const [messages, setMessages] = React.useState([]);
-  const subscriptionRef = React.useRef(null);
+  const { data, loading, error } = useQuery(messagesQuery);
+  const [addMessage] = useMutation(addMessageMutation);
 
-  React.useEffect(() => {
-    async function fetchMessages() {
-      const messages = await getMessages();
-      setMessages(messages);
-    }
-    fetchMessages();
-    return () => {
-      console.log("Unsubscribing chat events...");
-      subscriptionRef.current.unsubscribe();
-    };
-  }, []);
-
-  React.useEffect(() => {
-    subscriptionRef.current = onMessageAdded((message) =>
-      setMessages([...messages, message])
-    );
-  }, [messages]);
+  useSubscription(messageAddedSubscription, {
+    onSubscriptionData: ({
+      client,
+      subscriptionData: {
+        data: { messageAdded: message },
+      },
+    }) => {
+      client.writeData({
+        data: {
+          messages: [...messages, message],
+        },
+      });
+    },
+  });
 
   const handleSend = async (text) => {
-    await addMessage(text);
+    await addMessage({ variables: { input: { text } } });
   };
+
+  if (loading) {
+    return <div>Loading messages...</div>;
+  }
+
+  if (error) {
+    return <div style={{ color: "#ff0000" }}>Failed to load messages</div>;
+  }
+
+  const messages = data ? data.messages : [];
 
   return (
     <section className="section">
